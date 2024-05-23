f69428a19e6ef01a00286d6c7ab5c79802b3bd29
CahunH <116684250+CahunH@users.noreply.github.com>
2024-05-22 19:16:39 -0600

    app
    app

--- code.py
+++ code.py
@@ -1,2 +1,21 @@
-print("Hello World!")
-input (KeyError)
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    # lógica de login
+    pass
+
+@app.route('/add_password', methods=['POST'])
+def add_password():
+    # lógica para añadir contraseña
+    pass
+
+@app.route('/get_password', methods=['GET'])
+def get_password():
+    # lógica para recuperar contraseña
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
